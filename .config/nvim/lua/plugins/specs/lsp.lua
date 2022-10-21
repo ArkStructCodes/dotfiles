405322b3580d57f69205ecf2768f1b9eef2dbad7
FernOfSigma <90320595+FernOfSigma@users.noreply.github.com>
2022-10-21 10:03:40 +0530

    Update To Comply With Breaking Changes In Plugins

--- .config/nvim/lua/plugins/specs/lsp.lua
+++ .config/nvim/lua/plugins/specs/lsp.lua
@@ -37,10 +37,7 @@ return {
                 'clangd', 'rust_analyzer' 
             }
 
-            local capabilities = require('cmp_nvim_lsp').update_capabilities(
-                vim.lsp.protocol.make_client_capabilities()
-            )
-
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             for i = 1, #servers do
                 require('lspconfig')[servers[i]].setup {
                     capabilities = capabilities
--- .config/nvim/lua/plugins/specs/treesitter.lua
+++ .config/nvim/lua/plugins/specs/treesitter.lua
@@ -1,51 +1,13 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    requires = 'nvim-treesitter/nvim-treesitter-textobjects',
     config = function()
         require('nvim-treesitter.configs').setup {
             ensure_installed = {
-                'bash', 'c', 'javascript', 'lua', 'python', 'rust',
+                'bash', 'c', 'cpp', 'javascript', 'lua', 'python', 'rust',
                 'html', 'css', 'markdown', 'json', 'yaml', 'toml'
             },
             highlight = { enable = true },
-            indent = { enable = true },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner'
-                    },
-                    selection_modes = {
-                        ['@parameter.outer'] = 'v', -- charwise
-                        ['@function.outer'] = 'V', -- linewise
-                        ['@class.outer'] = '<c-v>', -- blockwise
-                    }
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true, -- Set jumps in the jumplist
-                    goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
-                    },
-                    goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
-                    },
-                    goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
-                    },
-                    goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
-                    }
-                }
-            }
+            indent = { enable = true }
         }
     end,
     run = ':TSUpdate',
