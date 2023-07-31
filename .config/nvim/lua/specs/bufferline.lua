b52f5b59f8fb4b690e28457a50e8c439a9e18571
FernOfSigma <90320595+FernOfSigma@users.noreply.github.com>
2023-07-31 11:02:29 +0530

    Improved neovim load times

--- .config/nvim/lua/specs/bufferline.lua
+++ .config/nvim/lua/specs/bufferline.lua
@@ -1,13 +1,20 @@
 return {
     "akinsho/bufferline.nvim",
     version = "*",
-    requires = "kyazdani42/nvim-web-devicons",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("bufferline").setup {}
+        local bufferline = require("bufferline")
+        bufferline.setup({
+            options = {
+                show_close_icon = false,
+                show_buffer_close_icons = false,
+                hover = { enabled = false },
+            }
+        })
 
         -- Set keybinds for navigation.
         noremap("n", "<Leader>n", ":BufferLineCycleNext<CR>")
         noremap("n", "<Leader>b", ":BufferLineCyclePrev<CR>")
         noremap("n", "<Leader>c", ":BufferLinePickClose<CR>")
-    end
+    end,
 }
--- .config/nvim/lua/specs/lsp.lua
+++ .config/nvim/lua/specs/lsp.lua
@@ -7,42 +7,36 @@ SIGN_ICONS = {
     Info = " "
 }
 
+-- Set custom diagnostic signs for the sign column.
+for sign, icon in pairs(SIGN_ICONS) do
+    local hl = "DiagnosticSign" .. sign
+    vim.fn.sign_define(hl, {
+        text = icon,
+        texthl = hl,
+        numhl = hl
+    })
+end
+
+-- Change diagnostic text prefix.
+vim.diagnostic.config { virtual_text = { prefix = "▮ " }}
+
 return {
-    {
-        "neovim/nvim-lspconfig",
-        init = function()
-            -- Change diagnostic text prefix.
-            vim.diagnostic.config { virtual_text = { prefix = "▮ " }}
+    "hrsh7th/cmp-nvim-lsp",
+    dependencies = { "neovim/nvim-lspconfig" },
+    config = function()
+        local servers = {
+            -- pyright, typescript, typescript-language-server (npm)
+            "pyright", "tsserver",
+            -- clang, rust-analyzer (native package)
+            "clangd", "rust_analyzer" 
+        }
 
-            -- Set custom diagnostic signs for the sign column.
-            for sign, icon in pairs(SIGN_ICONS) do
-                local hl = "DiagnosticSign" .. sign
-                vim.fn.sign_define(hl, {
-                    text = icon,
-                    texthl = hl,
-                    numhl = hl
-                })
-            end
-        end,
-        ft = LSP_FILETYPES
-    },
-    {
-        "hrsh7th/cmp-nvim-lsp",
-        dependencies = { "nvim-lspconfig" },
-        config = function()
-            local servers = {
-                -- pyright, typescript, typescript-language-server (npm)
-                "pyright", "tsserver",
-                -- clang, rust-analyzer (native package)
-                "clangd", "rust_analyzer" 
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        for i = 1, #servers do
+            require("lspconfig")[servers[i]].setup {
+                capabilities = capabilities
             }
-
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            for i = 1, #servers do
-                require("lspconfig")[servers[i]].setup {
-                    capabilities = capabilities
-                }
-            end
-        end,
-    }
+        end
+    end,
+    ft = LSP_FILETYPES,
 }
