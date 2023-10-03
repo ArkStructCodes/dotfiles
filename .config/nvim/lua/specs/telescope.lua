688859defa41fa646a28496e8ecfb5bd19c50260
FernOfSigma <90320595+FernOfSigma@users.noreply.github.com>
2023-10-03 06:56:36 +0530

    Update local config

--- .config/nvim/lua/specs/telescope.lua
+++ .config/nvim/lua/specs/telescope.lua
@@ -3,25 +3,23 @@ return {
     tag = "0.1.2",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-file-browser.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
         local telescope = require("telescope")
         local builtin = require("telescope.builtin")
 
-        telescope.load_extension("file_browser")
         telescope.load_extension("fzf")
 
+        map("n", "<Leader>ff", builtin.find_files)
         map("n", "<Leader>fg", builtin.live_grep)
         map("n", "<Leader>fb", builtin.buffers)
         map("n", "<Leader>fh", builtin.help_tags)
-        map("n", "<Leader>ff", telescope.extensions.file_browser.file_browser)
     end,
     keys =  {
+        { "<Leader>ff", mode = "n" },
         { "<Leader>fg", mode = "n" },
         { "<Leader>fb", mode = "n" },
         { "<Leader>fh", mode = "n" },
-        { "<Leader>ff", mode = "n" },
     },
 }
